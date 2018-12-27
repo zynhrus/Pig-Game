c74f0115250e8af01dd20e497d3197503a6cad2e
Albert <albertonly898@gmail.com>
2018-12-27 20:28:16 +0700

    100

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     //  Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.getElementById('name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
