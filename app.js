7fefa5f16c7fc1af1b095a4789a5077605311644
Albert <albertonly898@gmail.com>
2018-12-28 09:36:25 +0700

    display none

--- app.js
+++ app.js
@@ -81,6 +81,8 @@ function init() {
   activePlayer = 0;
   gamePlaying = true;
 
+  document.querySelector('.dice').style.display = 'none';
+
   document.getElementById("score-0").textContent = 0;
   document.getElementById("score-1").textContent = 0;
   document.getElementById("current-0").textContent = 0;
